2ccd3b680700fa955710caa3efb30d32c922d9d1
sengvonkh@gmail.com <sengvonkh@gmail.com>
2025-01-29 21:53:10 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,19 @@
 import React from "react";
 import Link from "next/link";
 import Sidebar from "./_Sidebar";
+import "./globals.css";
 
 export const metadata = {
   title: "nib-pos-project",
-  description: "Sale/Rentals Management System"
+  description: "Sale/Rentals Management System",
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  return(
+  return (
     <html lang="en">
       <body>
         <div style={{ display: "flex" }}>
@@ -21,12 +22,21 @@ export default function RootLayout({
           <div style={{ marginLeft: "14%", width: "86%", padding: "10px" }}>
             <header>
               <div>
-                <div style={{display: "flex", backgroundColor: "#9EEEA6", height: 50, paddingBottom: 20}}>
-                  <div style={{paddingRight: 30, paddingLeft: 20}}>
+                <div
+                  style={{
+                    display: "flex",
+                    backgroundColor: "#9EEEA6",
+                    height: 50,
+                    alignItems: "center",
+                  }}
+                >
+                  <div style={{ paddingRight: 30, paddingLeft: 20 }}>
                     <h1>KHCode</h1>
                   </div>
-                  <div style={{display: "flex", paddingRight: 100, marginTop: 10, gap: 10}}>
-                    <h3><Link href={"/"}>Home</Link></h3>
+                  <div>
+                    <h3>
+                      <Link href={"/"}>Home</Link>
+                    </h3>
                   </div>
                 </div>
               </div>
